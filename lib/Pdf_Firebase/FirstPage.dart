9833a15653d877bc54010bee5abe5023d7455d9e
deepanshusaini199 <sainids785@gmail.com>
2023-06-04 23:57:55 +0530

    initialized the firebase console for storage

---
+++ lib/Pdf_Firebase/FirstPage.dart
@@ -0,0 +1,44 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:pdf_viewer_plugin/pdf_viewer_plugin.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          getPdfAndUpload();
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+  Future getPdfAndUpload() async {
+    String randomName = DateTime.now().microsecondsSinceEpoch.toString();
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+    if (result == null) return null;
+    // return File(result.paths.first)
+  }
+}
